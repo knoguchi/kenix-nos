4d593e6e003f871d833e43812f331b21be79224e
Kenji Noguchi <tokyo246@gmail.com>
2014-01-12 18:06:06 -0800

    4 char indent for JS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 angular.module('nosApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('nosApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    })
+    .controller('HeaderController', function ($scope, $location) {
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+    });
